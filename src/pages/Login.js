2cf140686ad4792950ce478948e792f5b84f8a13
mijinB <bmj13465@gmail.com>
2023-08-05 16:31:31 +0900

    axios설치/login form setting

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Login() {
+  const [ email, setEmail ] = useState("");
+  const [ password, setPassword ] = useState("");
+
   return (
-    <div>Login</div>
+    <form>
+      {console.log(password)}
+      <input
+      required
+      value={email}
+      type="email"
+      placeholder="이메일을 작성해주세요."
+      onChange={ (event) => setEmail(event.target.value) }
+      />
+      <input
+      required
+      value={password}
+      type="password"
+      placeholder="비밀번호를 작성해주세요."
+      onChange={ (event) => setPassword(event.target.value) }
+      />
+      <button>Log In</button>
+    </form>
   )
 }
