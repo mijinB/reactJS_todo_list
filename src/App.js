514ea634755c01182b4841e286df7bd3dc268e0e
mijinB <bmj13465@gmail.com>
2023-08-10 16:44:23 +0900

    CSS 파일 삭제

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Route, Routes } from 'react-router-dom';
-import './App.css';
 import SignUpComponent from './pages/SignUp';
 import SignInComponent from './pages/SignIn';
 import TodoComponent from './pages/Todo';
